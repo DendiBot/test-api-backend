9e0a38c580d4cd9d19cd6a7ea7a6383c75077f8d
DendiBot <sujithisathome@gmail.com>
2021-01-17 14:47:51 -0800

    Updated price model

--- models/coffee.js
+++ models/coffee.js
@@ -6,6 +6,10 @@ var coffeeSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    price: {
+        type: Number,
+        required: true
+    },
     deleted: {
         type: Boolean,
         default: false
